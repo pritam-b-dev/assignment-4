ffc1f810bbf062af7f5a5ae05048cbd7bea8010e
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-02-22 21:49:23 +0600

    toggling buttons using "for of" loop by add and remove the classes.

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function showTotalNumbersOfJobsSelectReject() {
   availJobs.innerText = jobCards.children.length;
 }
 showTotalNumbersOfJobsSelectReject();
+
+// toggle buttons
+let buttons = document.querySelectorAll("#btn-tab button");
+console.log(buttons);
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    for (btn of buttons) {
+      btn.classList.remove("bg-blue-500", "text-white");
+      btn.classList.add("bg-white", "text-[#64748B]");
+    }
+    button.classList.remove("bg-white", "text-[#64748B]");
+    button.classList.add("bg-blue-500", "text-white");
+  });
+}
